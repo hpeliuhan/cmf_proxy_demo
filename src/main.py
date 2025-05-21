bf2b3d40c199df1c93c53568147eed5383c90a09
hpeliuhan <han.liu@hpe.com>
2025-05-21 04:59:48 +0000

    change prediction name

--- src/main.py
+++ src/main.py
@@ -28,7 +28,7 @@ def process_video(video_path, input_size, smoke_threshold=0.5):
                     sample_path = "image.jpg"
                     sample.save(sample_path)
                     plugin.upload_file(sample_path, timestamp=sample.timestamp)
-                    plugin.publish("classification certainty", float(predictions[0][0]),
+                    plugin.publish("classification.certainty", float(predictions[0][0]),
                                     timestamp=sample.timestamp,
                                     meta={"camera": f'{camera_src}'})
                     
