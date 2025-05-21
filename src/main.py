bb620b428c77965b8a55d4fb634ea3fd3977a805
hpeliuhan <han.liu@hpe.com>
2025-05-21 07:40:31 +0000

    test uploaed

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ def main():
     with Plugin() as plugin:
         t=time.time()
         plugin.publish("helloworld", t)
+        plugin.upload_file("test.txt", t)
 
 if __name__ == "__main__":
     main()
