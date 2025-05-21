30527bc7278a22723327b098301eb7969b687548
hpeliuhan <han.liu@hpe.com>
2025-05-21 07:46:34 +0000

    test uploaed

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
 from waggle.plugin import Plugin
 import time
+from waggle.time import get_timestamp
 
 def main():
     with Plugin() as plugin:
         t=time.time()
         plugin.publish("helloworld", t)
-        plugin.upload_file("test.txt", t)
-
+        plugin.upload_file("test.txt",timestamp=get_timestamp())
 if __name__ == "__main__":
     main()
